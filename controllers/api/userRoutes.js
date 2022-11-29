dae680b835cda37449149a1957c3291f0814c5b0
jacknel <mjacknelson19@gmail.com>
2022-11-28 21:05:50 -0600

    updated routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -59,4 +59,6 @@ router.post('/logout', (req, res) => {
   }
 });
 
+
+
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
+const profileRoutes = require('./profile-routes')
 
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/profile', profileRoutes);
 
 
 module.exports = router;
