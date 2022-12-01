f91f4b111e53522b481fd4e5c3f4b8bca34e2633
jacknel <mjacknelson19@gmail.com>
2022-11-30 20:46:32 -0600

    made community routes and page functional

--- controllers/community-routes.js
+++ controllers/community-routes.js
@@ -2,41 +2,28 @@ const router = require('express').Router();
 const { User, Project } = require('../models');
 
 router.get('/', async (req, res) => {
-  // try {
-  //   const bottleData = await Project.findAll(req.params.id, {
-  //   });
-  //   const bottle = bottleData.get({ plain: true });
-  //   res.render('community', {
-  //     ...bottle,
-  //     logged_in: true
-  //   });
-  // } catch (error) {
-  //   console.log(error)
-  //   res.status(500).json(error);
-  // }
- 
-try {
-  // Get all projects and JOIN with user data
-  const projectData = await Project.findAll({
-    include: [
-      {
-        model: User,
-        attributes: ['name'],
-      },
-    ],
-  });
+  try {
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
 
-  // Serialize data so the template can read it
-  const bottle = bottletData.map((project) => project.get({ plain: true }));
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true }));
 
-  // Pass serialized data and session flag into template
-  res.render('community', { 
-    ...bottle, 
-    logged_in: true
-  });
-} catch (err) {
-  res
-}
+    // Pass serialized data and session flag into template
+    res.render('community', { 
+      projects, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
